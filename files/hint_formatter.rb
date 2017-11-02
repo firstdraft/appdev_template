7be3cd0ed4145dde2e6ff701cea843ef4543aaa5
Murugan <murugu2012@gmail.com>
2017-11-02 08:59:06 +0530

    Format hint

--- files/hint_formatter.rb
+++ files/hint_formatter.rb
@@ -4,8 +4,8 @@ class HintFormatter < RSpec::Core::Formatters::DocumentationFormatter
   RSpec::Core::Formatters.register self, :example_failed
 
   def example_failed(failure)
-    puts "Hint:  #{failure.example.metadata[:hint][0]}" if failure.example.metadata[:hint].present?
     super
+    @output.puts "\n\nHint:  #{failure.example.metadata[:hint][0]}" if failure.example.metadata[:hint].present?
   end
 
 end
