919c27a32ac642ec9676e4722dc7ca6c9b187528
Jelani Woods <jelani@firstdraft.com>
2019-10-23 16:23:28 -0500

    Small tweaks

--- template.rb
+++ template.rb
@@ -50,8 +50,9 @@ gem_group :development, :test do
   gem "dotenv-rails"
   gem "grade_runner", github: "firstdraft/grade_runner"
   gem "pry-rails"
-  gem "sqlite3", "~> 1.4.1"
+  gem "sqlite3", "~> 1.4"
   gem "table_print"
+  gem "faker"
   gem "web_git", github: "firstdraft/web_git"
 end
 
